e2651ae934bf03f7256ffe8ab1c8e40934b49370
Sarah Myriam <sarahmyriam@Sarahs-MacBook-Pro.local>
2021-01-18 15:18:35 -0500

    in the card folder created and index file and a function card for the header and the title

---
+++ bookslover/src/components/card/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Card({title,childre}){
+    return(
+        <div className="card mt-2">
+            <div className ="card-header">
+                <h3>{title}</h3>
+            </div>
+            <div className="card-body">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default Card;
